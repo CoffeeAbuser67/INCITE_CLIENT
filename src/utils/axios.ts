d36c19bf2ffd9fbd2b3db1a155df57a825c68c2d
CoffeeAbuser67 <Hservers170224@gmail.com>
2026-03-18 20:39:50 -0300

    muda o BASE_URL

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -4,13 +4,13 @@ import axios from "axios";
 // const BASE_URL = 'http://127.0.0.1:81/api/v1';
 
 // _PIN_ Local Server : 'http://127.0.0.1:8000/api/v1';
-const BASE_URL = "http://127.0.0.1:8000/api/v1";
+// const BASE_URL = "http://127.0.0.1:8000/api/v1";
 
 // _PIN_ Production Server :
 // const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 
-// const BASE_URL = '/api/v1';
+const BASE_URL = '/api/v1';
 
 
 // Instancia axios criada para usar no Interceptor
